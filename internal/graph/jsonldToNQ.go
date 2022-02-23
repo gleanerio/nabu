8644c1fe8fc15188f661ffdd9bfe6932f15abe53
Douglas Fils <drfils@gmail.com>
2022-02-23 01:05:08 -0600

    working on quads with graph aligned with Gleaner data approaches

--- internal/graph/jsonldToNQ.go
+++ internal/graph/jsonldToNQ.go
@@ -3,8 +3,9 @@ package graph
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/piprate/json-gold/ld"
 	"log"
+
+	"github.com/piprate/json-gold/ld"
 )
 
 // JSONLDToNQ takes JSON-LD and convets to nqquads (or ntriples if no graph?)
@@ -20,14 +21,12 @@ func JSONLDToNQ(jsonld string) (string, error) {
 	if err != nil {
 		log.Println("Error when transforming JSON-LD document to interface:", err)
 		return "", err
-
 	}
 
 	triples, err := proc.ToRDF(myInterface, options) // returns triples but toss them, just validating
 	if err != nil {
 		log.Println("Error when transforming JSON-LD document to RDF:", err)
 		return "", err
-
 	}
 
 	return fmt.Sprintf("%v", triples), err
---
+++ internal/graph/nqNewGraph.go
@@ -0,0 +1,62 @@
+package graph
+
+import (
+	"bytes"
+	"errors"
+	"log"
+	"strings"
+
+	"github.com/knakk/rdf"
+)
+
+//NQtoNTGraph converts nquads to nquads with a new context graph
+func NQToNTGraph(inquads, newctx string) (string, string, error) {
+	// loop on tr and make a set of triples
+	ntr := []rdf.Triple{}
+	g := ""
+
+	dec := rdf.NewQuadDecoder(strings.NewReader(inquads), rdf.NQuads)
+	tr, err := dec.DecodeAll()
+	if err != nil {
+		log.Printf("Error decoding triples: %v\n", err)
+		return "", g, err
+	}
+
+	// check we have triples
+	if len(tr) < 1 {
+		return "", g, errors.New("no triple")
+	}
+
+	for i := range tr {
+		ntr = append(ntr, tr[i].Triple)
+	}
+
+	// Assume context of first triple is context of all triples  (again, a bit of a hack,
+	// but likely valid as a single JSON-LD datagraph level).  This may be problematic for a "stitegraphs" where several
+	// datagraph are represented in a single large JSON-LD via some collection concept.  There it is possible someone might
+	// use the quad.  However, for most cases the quad is not important to us, it's local provenance, so we would still replace
+	// it with our provenance (context)
+	ctx := tr[0].Ctx
+	g = ctx.String()
+
+	// TODO make a new Ctx from newCtx and repate the existing one
+
+	// TODO update the following to output quads
+
+	// TODO output
+	outtriples := ""
+	buf := bytes.NewBufferString(outtriples)
+	enc := rdf.NewTripleEncoder(buf, rdf.NTriples)
+	err = enc.EncodeAll(ntr)
+	if err != nil {
+		log.Printf("Error encoding triples: %v\n", err)
+	}
+	enc.Close()
+
+	tb := bytes.NewBuffer([]byte(""))
+	for k := range ntr {
+		tb.WriteString(ntr[k].Serialize(rdf.NTriples))
+	}
+
+	return tb.String(), g, err
+}
