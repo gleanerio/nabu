682c4f479966edddace9c6cb51865e2e9788efb6
Douglas Fils <drfils@gmail.com>
2023-09-01 08:18:25 -0500

    Aligning urn.go to the 0001-URN-decision.md guidance

--- decisions/0002-objectPrefixes-decision.md
+++ decisions/0002-objectPrefixes-decision.md
@@ -9,17 +9,25 @@ Proposed
 ## Context
 
 There are some conventions used in the levering of an object store by GleanerIO.
-This ADR scopes the naming conventions used both by Gleaner and Nabu
+This ADR scopes the naming conventions used both by Gleaner and Nabu.
 
-* archive/
+Some of these conventions have implications on the behavior of the code.  For
+example, the URN generation leverages the path structure to establish the 
+urn structure (see 0001-URN-decision.md).  
+
+Though the resulting URN is abstracted from the object prefix value, that prefix 
+is still used in the initial formation.  
+
 * graphs/
+  * graphs/archive
+  * graphs/latest
+  * graphs/summary
+* summoned/
+* prov/
 * milled/
 * orgs/
-* prov/
 * reports/
 * scheduler/
-* scratch/
-* summoned/
 
 ## Decision
  
--- scripts/simpletest/testset.sh
+++ scripts/simpletest/testset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NABU='go run ../../cmd/nabu/main.go'
-CFG='../../secret/iow/iow_dev_v4.yaml'
+CFG='../../xxx/iow/iow_dev_v4.yaml'
 BLAZE='http://coreos.lan:9090/blazegraph/namespace/iow/sparql'
 GRAPHDB='http://coreos.lan:7200/repositories/testing'
 
@@ -55,3 +55,34 @@ ${NABU} prefix --cfg ${CFG}  --prefix summoned/test --endpoint iow_graphdb
 ${NABU} prune --cfg ${CFG}  --prefix summoned/test --endpoint iow_graphdb
 
 curl -H 'Accept: application/sparql-results+json' ${GRAPHDB} --data-urlencode 'query=SELECT (COUNT(DISTINCT ?graph) AS ?namedGraphsCount)(COUNT(*) AS ?triplesCount)WHERE {GRAPH ?graph {?subject ?predicate ?object}}' | jq '.results.bindings[0] | { namedGraphsCount: .namedGraphsCount.value, triplesCount: .triplesCount.value }' input.json -
+
+
+
+
+echo "-------  BLAZEGRAPH PROV"
+echo "----------  clear, bulk, prune "
+${NABU} clear --cfg ${CFG} --dangerous --endpoint ec_blazegraph
+${NABU} bulk --cfg ${CFG}  --prefix prov/counties0 --endpoint ec_blazegraph
+${NABU} prune --cfg ${CFG}  --prefix prov/counties0 --endpoint ec_blazegraph
+
+curl -H 'Accept: application/sparql-results+json' ${BLAZE} --data-urlencode 'query=SELECT (COUNT(DISTINCT ?graph) AS ?namedGraphsCount)(COUNT(*) AS ?triplesCount)WHERE {GRAPH ?graph {?subject ?predicate ?object}}' | jq '.results.bindings[0] | { namedGraphsCount: .namedGraphsCount.value, triplesCount: .triplesCount.value }' input.json -
+
+
+echo "----------  clear, release, object, prune "
+${NABU} clear --cfg ${CFG} --dangerous --endpoint ec_blazegraph
+${NABU} release --cfg ${CFG}  --prefix prov/counties0 --endpoint ec_blazegraph
+${NABU} object --cfg ${CFG}  graphs/latest/test_release.nq --endpoint ec_blazegraph
+${NABU} prune --cfg ${CFG}  --prefix prov/counties0 --endpoint ec_blazegraph
+
+curl -H 'Accept: application/sparql-results+json' ${BLAZE} --data-urlencode 'query=SELECT (COUNT(DISTINCT ?graph) AS ?namedGraphsCount)(COUNT(*) AS ?triplesCount)WHERE {GRAPH ?graph {?subject ?predicate ?object}}' | jq '.results.bindings[0] | { namedGraphsCount: .namedGraphsCount.value, triplesCount: .triplesCount.value }' input.json -
+
+
+echo "----------  clear, prefix, prune "
+${NABU} clear --cfg ${CFG} --dangerous --endpoint ec_blazegraph
+${NABU} prefix --cfg ${CFG}  --prefix prov/counties0 --endpoint ec_blazegraph
+${NABU} prune --cfg ${CFG}  --prefix prov/counties0 --endpoint ec_blazegraph
+
+curl -H 'Accept: application/sparql-results+json' ${BLAZE} --data-urlencode 'query=SELECT (COUNT(DISTINCT ?graph) AS ?namedGraphsCount)(COUNT(*) AS ?triplesCount)WHERE {GRAPH ?graph {?subject ?predicate ?object}}' | jq '.results.bindings[0] | { namedGraphsCount: .namedGraphsCount.value, triplesCount: .triplesCount.value }' input.json -
+
+
+
